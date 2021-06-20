a4d7e2df1e73b973a2cddf5f9f4c9ca2195035dc
Arazmyrat <arazmirat@icloud.com>
2021-06-20 04:18:27 +0400

    add crud methods

--- mongoose-sample/routes/post.Route.js
+++ mongoose-sample/routes/post.Route.js
@@ -3,6 +3,27 @@ var router = express.Router();
 
 const PostModel = require("../models/Post");
 
+router.get('/sorting', (req, res) => {
+  PostModel.find()
+                .sort({id:'descending',title:1,"meta.vote":-1})/**descending:-1:desc ||| ascending:1:asc */
+               .then((resultData)=>{res.json(resultData)})
+               .catch((errorDetail)=>{res.json(errorDetail)}) 
+
+          
+})
+
+router.get('/limit-skip', (req, res) => {
+  PostModel.find({},'title isActive')
+              //.sort({id:'descending'})
+              .skip(1)
+              .limit(2)
+               .then((resultData)=>{res.json(resultData)})
+               .catch((errorDetail)=>{res.json(errorDetail)}) 
+
+          
+})
+
+
 router.get('/search', (req, res) => {
    /* PostModel.find({isActive:false,title: "Test Title"})
                 .then((resultData)=>{res.json(resultData)})
@@ -24,7 +45,7 @@ router.get('/searchBy', (req, res) => {
 
  router.get('/searchById', (req, res) => {
  
-    PostModel.findById('60ca40dbbac9cb3bf45abcac','isActive title', (errorDetail, resultData) => {
+    PostModel.findById('60ca376cf4c52d4227d72aef','isActive title', (errorDetail, resultData) => {
       if (errorDetail) {res.json(errorDetail);}
       res.json(resultData);
     });         
@@ -51,7 +72,7 @@ router.post("/", (req, res) => {
 
 router.get("/", (req, res) => {
   const _post = new PostModel({
-     title: "Test Title",
+     title: "Test Title last",
     id: 11,
     body: "Body Content Required",
     isActive: false,
@@ -65,6 +86,59 @@ router.get("/", (req, res) => {
     res.json(data);
   });
   //res.send('GET request to the homepage')
+});
+
+/**UPDATEMANY() */
+router.put('/updatemany', function(req, res) {
+  PostModel.updateMany({isActive:false},{title:'Title updated.'},(err,data)=>
+  {
+    if(err) res.json(err)
+    res.json(data)
+  })
+});
+
+/**UPDATEONE() */
+router.put('/updateone', function(req, res) {
+  PostModel.updateOne({isActive:false},{title:'Title updated from updateOne Method.'},(err,data)=>
+  {
+    if(err) res.json(err)
+    res.json(data)
+  })
+});
+
+/**FINDBYIDANDUPDATE() */
+router.put('/findbyidandupdate', function(req, res) {
+  PostModel.findByIdAndUpdate('60ca37a9d8380c4285c05281',
+  {title:'Title updated from findByIdAndUpdate Method.','meta.favs':99}
+  ,(err,data)=>{if(err) res.json(err)
+    res.json(data)  })
+});
+
+/**DELETEMANY() */
+router.delete('/deletemany', function(req, res) {
+  PostModel.deleteMany({isActive:false},(err,data)=>
+  {
+    if(err) res.json(err)
+    res.json(data)
+  })
+});
+
+/**DELETEONE() */
+router.delete('/deleteone', function(req, res) {
+  PostModel.deleteOne({isActive:true},(err,data)=>
+  {
+    if(err) res.json(err)
+    res.json(data)
+  })
+});
+
+/**FINDBYIDANDREMOVE() */
+router.delete("/findbyidandremove", function (req, res) {
+  var postId = "60ca48cfa829d13a685b9c2e";
+  PostModel.findByIdAndRemove(postId, (err, data) => {
+    if (err) res.json(err);
+    res.json("Removed Post : " + data);
+  });
 });
 
 module.exports = router;
